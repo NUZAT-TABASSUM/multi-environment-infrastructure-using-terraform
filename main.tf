d4cf688dd9036fcf5361b801fef4a4b3cfecf51a
Nuzat Tabassum <115428464+NUZAT-TABASSUM@users.noreply.github.com>
2023-12-01 00:54:49 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,3 +23,16 @@ module "stg-app" {
     ami = "ami-007855ac798b5175e" 
   
 }
+# modules/autoscaling/main.tf
+variable "min_instances" {}
+variable "max_instances" {}
+
+resource "aws_autoscaling_group" "example" {
+  // Auto-scaling group configurations
+}
+
+module "autoscaling" {
+  source        = "./modules/autoscaling"
+  min_instances = 2
+  max_instances = 5
+}
