702c4bec5dfe4921ab88357561ddf84806124616
NUZAT TABASSUM <nuzattabassum74@gmail.com>
2023-11-30 23:26:55 +0100

    changes

--- main.tf
+++ main.tf
@@ -23,3 +23,33 @@ module "stg-app" {
     ami = "ami-007855ac798b5175e" 
   
 }
+
+# Define Auto Scaling Group
+resource "aws_autoscaling_group" "example" {
+  desired_capacity     = 2
+  max_size             = 5
+  min_size             = 1
+
+  # Other auto-scaling configurations go here...
+
+  launch_configuration = aws_launch_configuration.example.id
+}
+
+ lifecycle {
+    create_before_destroy = true
+  }
+
+variable "asg_desired_capacity" {
+  description = "Auto Scaling Group desired capacity"
+  default     = 2
+}
+
+variable "asg_max_size" {
+  description = "Auto Scaling Group maximum size"
+  default     = 5
+}
+
+variable "asg_min_size" {
+  description = "Auto Scaling Group minimum size"
+  default     = 1
+}
